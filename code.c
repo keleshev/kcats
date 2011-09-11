2fe9fc75dc63614f6722cb64d80cffd9271ed8e9
Vladimir Keleshev <halst@ya.ru>
2011-09-12 00:04:48 +0200

    Now you can define functions!

--- code.c
+++ code.c
@@ -1,5 +1,21 @@
 
 
-char code[] = "abs: 2 ^ 0.5 ^";
+char code[] = "\n"
+"max: [2] [2] > [3] [3] swp > [3] * swp [4] * + swp x swp x return \n"
+"e: 2.71822 return \n"
+"!=: == ! return \n"
+"<: swp >"
+
+;
+
+
+char *find_def(char *word, char *code) {
+    char def[sizeof(word) + 2];
+    sprintf(def, "\n%s:", word);
+       // printf();
+    return strstr(code, def) + strlen(def);
+}       
+
+
 //char code[] = "e: 2.71822 ";
 //char code[] = "!=: == !":
--- kcats.c
+++ kcats.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>            
+#include <string.h> 
+#include <ctype.h>
 #include <math.h>
+#include <iso646.h>
+
 #include "stack.c"
 #include "code.c"
 
@@ -16,19 +19,31 @@ bool seq(char s1[], char s2[]) {  // string equal
 }
 
 bool is_definition(char w[]) {
-    int len = strlen(w);     
-    if((len > 1) && (w[len-1] == ':')) return true;
+    int l = strlen(w);     
+    if((l>1) and (w[l-1]=='{' or w[l-1]==':')) return true;
     else return false;
 }
 
-void eval_word(char word[]) {
-    char w[80]; strcpy(w, word);
+char *next_cmd(char f[], char s[]) {
+    int i = 0, j = 0;
+    while(s[i] != '\0') {
+        if(not isspace(s[i])) f[j++] = s[i];
+        else if(j > 0) break;
+        i++;
+    }
+    f[j] = '\0';
+    return &s[i];
+}
+
+bool eval_word(char w[]) {
     se_t number;
-    unsigned int index; 
+    char *p;
+    unsigned index; 
     if(sscanf(w, "%lf", &number) == 1) stack_push(number);
     else if(sscanf(w, "[%u]", &index) == 1) stack_push(stack_read(index));
-    else if(seq(w, "")) printf("oh, hai");
+    //else if(seq(w, "")) printf("oh, hai");
     else if(seq(w, "q")) exit(0);
+    else if(seq(w, "return")) return false;
     else if(seq(w, "c")) stack_clear();
     else if(seq(w, "swp")) stack_swap();
     else if(seq(w, "x")) stack_pop();
@@ -39,31 +54,34 @@ void eval_word(char word[]) {
     else if(seq(w, ">")) stack_infix(>);
     else if(seq(w, "!")) stack_prefix_1(!);
     else if(seq(w, "and")) stack_infix(&&);
+    else if((p = find_def(w, code)) != NULL) eval(p);
     else printf(RED "%s? \n" DEFAULT, w); 
+    return true;
 }
 
-void eval(char s[]) {
-    char tmp[80]; 
-    strcpy(tmp, s);
-    char *word = strtok(tmp, " \t\n");
-                //printf("<%s,%c>",tmp);
-//    strlen(word);
-    //printf("{%s}{%s}{%c}", word, s, word[strlen(word)-1]);
-    if(is_definition(tmp)) printf("Unimplemented\n");
+void eval(char sentence[]) {
+    char next[80]; //, rest[80];
+    char *rest;
+    rest = next_cmd(next, sentence);
+                        //printf("[%s|%s]",next,rest);
+    if(*next == '\0') return;
+    
+    if(is_definition(next)) {
+        printf("Unimplemented \n");
+    }
     else {
-        eval_word(tmp);
-        //eval(rest);
+        if(eval_word(next)) eval(rest);
     }
 }
-
+     
 int main() {
+
     char buffer[80];
+
     while(1) {
         stack_print();
         printf(YELLOW "> " DEFAULT);  // ➤
         fgets(buffer, 80, stdin); 
-        //if(seq(buffer, "\n")) {printf("cont"); continue;}
-        //printf("{%c|%c|%s}", buffer[0], buffer[1], buffer);
         eval(buffer);
     }
 }
