4e28a3b0a53770fd5976e086e68b505924cc7196
Vladimir Keleshev <halst@ya.ru>
2011-12-28 18:08:29 +0100

    Now you can execute .kc files.

--- kcats.c
+++ kcats.c
@@ -9,10 +9,21 @@
 
 //#define err(...) do { printf(RED); printf(__VA_ARGS__); printf(DEFAULT); } while (0)
      
-int main() {
+int main(int argc, char *argv[]) {
+
     stack st = stack_new();
     char* input;
     char* context; 
+    
+    if (argc == 2) {
+        input = str_from_file_new(argv[1]);
+        context = str_from_file_new("std.kc");
+        
+        st = eval(input, context, st);
+
+        free(input);
+        free(context);
+    }
 
     while (true) {
         printf(YELLOW);
